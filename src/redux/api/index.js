381c6dcaad7db906087288c8414d21fdba1764e1
juan.ortiz <juan.ortiz@digitalonus.com>
2019-07-25 22:41:06 -0500

    Update http

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = "http://www.omdbapi.com/?apikey=ffd0c3a5";
+const baseUrl = "https://www.omdbapi.com/?apikey=ffd0c3a5";
 
 export const apiCall = (url, data, headers, method) => axios({
     method,
